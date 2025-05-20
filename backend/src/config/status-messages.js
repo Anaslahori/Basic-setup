dd38a70988f4f72c198b57288df4f57c9d5043c8
Swapnil Joshi <swapnil.joshi@habilelabs.io>
2025-05-20 16:06:14 +0530

    fix: reorder message

--- backend/src/config/status-messages.js
+++ backend/src/config/status-messages.js
@@ -1,9 +1,9 @@
 module.exports = {
-  MISSING_DETAILS: "Required details are missing.",
-  USER_ALREADY_EXIST: "User already exists.",
-  OPERATION_FAILURE: "Something went wrong. Please try again.",
-  USER_NOT_EXIST: "User does not exist.",
-  INVALID_DETAILS: "Invalid details provided.",
-  INVALID_PASSWORD: "Incorrect password.",
   LOGIN_SUCCESS: "Login successful.",
+  INVALID_PASSWORD: "Incorrect password.",
+  INVALID_DETAILS: "Invalid details provided.",
+  MISSING_DETAILS: "Required details are missing.",
+  OPERATION_FAILURE: "Something went wrong. Please try again.",
+  ALREADY_EXIST: "Entry already exists.",
+  NOT_EXIST: "Entry does not exist.",
 };
